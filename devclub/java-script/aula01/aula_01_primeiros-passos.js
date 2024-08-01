1381ad285971faee85dff992be7092b9e0e31ee5
Everton Luis <130626846+evertonluispro@users.noreply.github.com>
2024-07-31 23:09:11 -0300

    Create aula_01_primeiros-passos.js

---
+++ devclub/java-script/aula01/aula_01_primeiros-passos.js
@@ -0,0 +1,15 @@
+/*
+1. Lógica de Programação -> Aprender como o computador pensa e aprender como se comunicar com ele
+
+Algoritmo para fazer Café:
+
+SE a cafeteira estiver ligada na tomada, seguir para o próximo passo.
+SE NÃO, ligá-la na tomada.
+
+SE o compartimento de água estiver cheio, seguir para o próximo passo.
+SE NÃO, o encha de aguá.
+
+SE o compartimento já tiver uma cápsula, seguir para o próximo passo.
+SE NÃO, coloque uma cápsula no compartimento.
+
+*/
