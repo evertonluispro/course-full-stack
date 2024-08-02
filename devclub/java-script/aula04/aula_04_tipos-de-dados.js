0a733a0b3da4bd31bc85b31950a23d878b3c55e4
Everton Luis <130626846+evertonluispro@users.noreply.github.com>
2024-08-01 22:50:06 -0300

    Create aula_04_tipos-de-dados.js

---
+++ devclub/java-script/aula04/aula_04_tipos-de-dados.js
@@ -0,0 +1,11 @@
+/*
+1. String -> Textos.
+*/
+
+const texto1 = "Textos com aspas duplas"
+const texto2 = 'Textos com aspas'
+const texto3 = `texto com crase`
+
+console.log(texto1)
+console.log(texto2)
+console.log(texto3)
