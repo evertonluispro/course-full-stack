78c94702df4d060fd50439b2870c4c848b703831
Everton Luis <130626846+evertonluispro@users.noreply.github.com>
2024-08-02 00:25:20 -0300

    Create aula_05_tipos-de-dados-parte2.js

---
+++ devclub/java-script/aula05/aula_05_tipos-de-dados-parte2.js
@@ -0,0 +1,12 @@
+/*
+1. String -> Textos.
+*/
+
+
+const myAge = 25
+const myCar = 'Audi'
+
+const myStrings = `Minha idade é ${myAge} anos, to ficando velho.
+Mas pelo menos estou andando de ${myCar}.` // Template Literals / Template String
+
+console.log(myStrings)
