705b602adf0a290ca97b8279140007e56e8dec9a
Everton Luis <130626846+evertonluispro@users.noreply.github.com>
2024-08-08 22:51:45 -0300

    Create aula_09_null-undefined.js

---
+++ devclub/java-script/aula09/aula_09_null-undefined.js
@@ -0,0 +1,13 @@
+/*
+1. Strings -> Textos.
+2. Numbers -> Números
+3. Boolean -> boleano
+4. Object -> Objetos
+5. Null & Undefined
+*/
+
+const user = {
+    name: "Rodolfo",
+    age: 30,
+    nomeDoConjuge: null
+}
