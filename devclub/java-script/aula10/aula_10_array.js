c3bf0dfdfd46a0b5608fdac6920460d9670b79fe
Everton Luis <130626846+evertonluispro@users.noreply.github.com>
2024-08-13 21:39:44 -0300

    Create aula_10_array.js

---
+++ devclub/java-script/aula10/aula_10_array.js
@@ -0,0 +1,33 @@
+/*
+1. Strings -> Textos.
+2. Numbers -> Números
+3. Boolean -> boleano
+4. Object -> Objetos
+5. Null & Undefined
+
+
+Estrutura de Dados
+
+1.Array
+*/
+
+const myArray = [
+  {
+    nome: "Rodolfo",
+    idade: 30,
+    nomeDoConjuge: "Aline",
+    id: null,
+  },
+  {
+    nome: "Aline", 
+    idade: 27,
+    nomeDoConjuge: "Rodolfo",
+    id: null,
+  },
+];
+
+const users = [20, 30, 490]
+
+users[2] = 454
+
+console.log(users[2])
