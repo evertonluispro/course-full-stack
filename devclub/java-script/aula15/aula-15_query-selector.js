586baba2a112c005198955313844d17168f1284e
Everton Luis <130626846+evertonluispro@users.noreply.github.com>
2024-08-20 21:28:13 -0300

    Create aula-15_query-selector.js

---
+++ devclub/java-script/aula15/aula-15_query-selector.js
@@ -0,0 +1,16 @@
+/*
+
+document -> HTML
+
+getElementById -> Trás UM elemento pelo ID
+getElementByClassName -> Trás TODOS os elementos com essa classe
+getElementByTagName -> Trás TODOS os elementos com essa TAG
+getElementByName -> Trás TODOS os elementos com esse NAME
+
+querySelector -> Trás UM elemento, o PRIMEIRO que encontrar
+querySelectorAll -> Trás TODOS os elementos que encontrar
+*/
+
+const elements = document.querySelector("button.main-button")
+
+console.log(elements)
