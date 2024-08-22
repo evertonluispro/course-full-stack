b4621ba4f613d15128ca8a6af807fe63c12e1fcd
Everton Luis <130626846+evertonluispro@users.noreply.github.com>
2024-08-21 22:02:20 -0300

    Create aula_19_eventos.js

---
+++ devclub/java-script/aula19/aula_19_eventos.js
@@ -0,0 +1,10 @@
+const input = document.querySelector("#main-input")
+const element = document.querySelector(".new-paragraph")
+
+function cliqueiNoBotao() {
+    element.textContent = input.value
+}
+
+function digiteiNoInput() {
+    console.log("Digitei no input")
+}
