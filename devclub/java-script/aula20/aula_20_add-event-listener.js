e5e373eb74673902671a2aa5db38dd62776f1789
Everton Luis <130626846+evertonluispro@users.noreply.github.com>
2024-08-22 21:45:38 -0300

    Create aula_20_add-event-listener.js

---
+++ devclub/java-script/aula20/aula_20_add-event-listener.js
@@ -0,0 +1,9 @@
+const input = document.querySelector("#main-input")
+const select = document.querySelector("select")
+const button = document.querySelector(".main-button")
+
+function troqueiValor() {
+    console.log("Troquei de valor")
+}
+
+select.addEventListener("change", troqueiValor)
