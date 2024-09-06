35f6efdf220dc187848fd2eca8bdd7beeccdff02
Everton Luis <130626846+evertonluispro@users.noreply.github.com>
2024-09-05 21:57:12 -0300

    Create aula_21_math.js

---
+++ devclub/java-script/aula21/aula_21_math.js
@@ -0,0 +1,14 @@
+/*
+
+Math -> Matemática
+    - pow(2,2) / potência / 2² = 4
+    - sqrt(25,2) / raiz / √25
+    - PI
+    - ceil / teto -> arredondar pra cima
+    - floor / chão -> arredondar pra baixo
+
+    - random() / número aleatório entre 0 e 10
+*/
+
+const result = Math.random()
+console.log(result)
